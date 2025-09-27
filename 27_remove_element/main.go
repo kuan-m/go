6b7d344d12520ecb3d203e2a12961db27bdb64bd
kuanjoy <kuanyshmykyev@gmail.com>
2025-09-28 01:25:42 +0600

    feat: 27, 75

---
+++ 27_remove_element/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	nums := []int{0, 1, 2, 2, 3, 0, 4, 2}
+	removeElement(nums, 2)
+}
+
+func removeElement(nums []int, val int) int {
+	k := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+
+	return k
+}
