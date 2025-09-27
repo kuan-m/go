5a8b86d9570de1c98895670b2d6e6205d828c466
kuanjoy <kuanyshmykyev@gmail.com>
2025-09-27 21:56:53 +0600

    feat: tdd|lessons 1,2,3

---
+++ fundamentals/3_arrays_and_slices/sum_test.go
@@ -0,0 +1,60 @@
+package arraysandslices
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("compare 2 slices", func(t *testing.T) {
+		got := []int{1, 2, 3}
+		want := []int{1, 2, 4}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	// reflect.DeepEqual is a convenient way of comparing slices
+}
+
+func Sum(numbers []int) int {
+	var sum int
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
