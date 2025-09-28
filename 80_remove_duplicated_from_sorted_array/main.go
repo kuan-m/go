6bf42239a525b34b4510cd4b975fde1e31fde035
kuanjoy <kuanyshmykyev@gmail.com>
2025-09-29 00:04:07 +0600

    feat: 80 remove duplicates from sorted array II

---
+++ 80_remove_duplicated_from_sorted_array/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	nums := []int{1, 1, 1, 2, 2, 3}
+	fmt.Println(removeDuplicates(nums))
+}
+
+func removeDuplicates(nums []int) int {
+	// 1. подход с памятью O(n)
+	// var seen = map[int]int{}
+
+	// k := 0
+	// for _, num := range nums {
+	// 	if seen[num] < 2 {
+	// 		nums[k] = num
+	// 		seen[num]++
+	// 		k++
+	// 	}
+	// }
+
+	// 2. подход с памятью O(1)
+	k := 0
+
+	for _, num := range nums {
+		if k < 2 || num != nums[k-2] {
+			nums[k] = num
+			k++
+		}
+	}
+
+	fmt.Println(nums[:k])
+	return len(nums[:k])
+}
