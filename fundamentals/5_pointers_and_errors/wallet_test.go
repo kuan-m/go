6a2def48aa8a0aaab1ff3438725266d54c3c6a23
kuanjoy <kuanyshmykyev@gmail.com>
2025-10-15 21:23:45 +0600

    feat: pointers and errors

---
+++ fundamentals/5_pointers_and_errors/wallet_test.go
@@ -0,0 +1,69 @@
+package pointersanderrors
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+}
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) { // но сделав w *Wallet - напрямую не с копией, а с памятью
+	w.balance += amount // когда меняем так, то мы работаем с копией
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+
+	w.balance -= amount
+	return nil
+}
