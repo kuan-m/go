6128acb989045dad357245005220ca9bd7a6fd6b
kuanjoy <kuanyshmykyev@gmail.com>
2025-12-08 21:48:10 +0600

    feat: 344. Reverse String

---
+++ 344_Reverse_String/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/reverse-string/description/
+
+// Pattern: Two Pointers
+// Time: O(n) Space: O(1)
+func main() {
+	s := []byte{'h', 'e', 'l', 'l', 'o'}
+
+	reverseString(s)
+
+	fmt.Print(s)
+}
+
+func reverseString(s []byte) {
+	left, right := 0, len(s)-1
+
+	for left < right {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
+	}
+}
