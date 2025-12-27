5a5d9c861104d220e86c468a6a2cb1bcecd04db0
kuanjoy <kuanyshmykyev@gmail.com>
2025-12-28 01:09:05 +0600

    test: 15. 3Sum

---
+++ 15_3Sum/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestThreeSum(t *testing.T) {
+	nums := []int{-1, 0, 1, 2, -1, -4}
+
+	expected := [][]int{
+		{-1, -1, 2},
+		{-1, 0, 1},
+	}
+
+	result := threeSum(nums)
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("expected %v, got %v", expected, result)
+	}
+}
